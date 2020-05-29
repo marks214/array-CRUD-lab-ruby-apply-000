d450685f1d638e99c06c74b1a163d649d5959ecc
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-29 16:40:47 +0000

    Automatically backed up by Learn

--- spec/array_crud_spec.rb
+++ spec/array_crud_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "#create_an_empty_array" do
-  it "creates and returns and empty array" do 
+  it "creates and returns an empty array" do 
     expect(create_an_empty_array).to eq([])
   end 
 end
